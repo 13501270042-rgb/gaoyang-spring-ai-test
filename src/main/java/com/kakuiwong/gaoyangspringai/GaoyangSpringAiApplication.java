075544793f90f720ec7586f9f7a52912882226d0
gaoyang <785175223@qq.com>
2026-09-17 09:42:42 +0800

    加入jdbc上下文存储

--- src/main/java/com/kakuiwong/gaoyangspringai/GaoyangSpringAiApplication.java
+++ src/main/java/com/kakuiwong/gaoyangspringai/GaoyangSpringAiApplication.java
@@ -1,12 +1,10 @@
 package com.kakuiwong.gaoyangspringai;
 
-import org.springframework.ai.model.chat.memory.repository.jdbc.autoconfigure.JdbcChatMemoryRepositoryAutoConfiguration;
 import org.springframework.ai.model.chat.memory.repository.redis.autoconfigure.RedisChatMemoryRepositoryAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude = {
-        JdbcChatMemoryRepositoryAutoConfiguration.class,
         RedisChatMemoryRepositoryAutoConfiguration.class})
 public class GaoyangSpringAiApplication {
 
--- src/main/java/com/kakuiwong/gaoyangspringai/config/ChatClientConfig.java
+++ src/main/java/com/kakuiwong/gaoyangspringai/config/ChatClientConfig.java
@@ -9,6 +9,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,25 +17,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatClientConfig {
 
+    //使用内存
+    // @Bean
+    // public ChatMemory chatMemory() {
+    //     return MessageWindowChatMemory.builder()
+    //             .maxMessages(10)
+    //             .build();
+    // }
+
     @Bean
-    public ChatMemory chatMemory() {
+    public ChatMemory chatMemory(JdbcChatMemoryRepository chatMemoryRepository) {
         return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(chatMemoryRepository)
                 .maxMessages(10)
                 .build();
     }
 
     @Bean
-    public ChatClient weatherChatClient(OllamaChatModel chatModel) {
+    public ChatClient weatherChatClient(OllamaChatModel chatModel,ChatMemory chatMemory) {
         return ChatClient.builder(chatModel)
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory()).build())
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .defaultSystem("你是天气播报员,回答关于天气相关问题")
                 .build();
     }
 
     @Bean
-    public ChatClient mathChatClient(OllamaChatModel chatModel) {
+    public ChatClient mathChatClient(OllamaChatModel chatModel,ChatMemory chatMemory) {
         return ChatClient.builder(chatModel)
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory()).build())
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .defaultSystem("你是数学老师,回答关于数学相关问题")
                 .build();
     }
